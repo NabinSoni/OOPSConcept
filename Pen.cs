24056318b923fa863da32178c9b3b1426ec3037c
Nabin Soni <nabinkumar570@gmail.com>
2022-05-15 11:32:24 +0530

    Initial Commit

---
+++ Pen.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPSConcept
+{
+    internal class Pen
+    {
+        public int PenId { get; set; }
+        public string PenType { get; set; }
+        public string PenColor { get; set; }
+
+        //public void penInfo()
+        //{
+        //    Console.WriteLine($"Pen Id is {PenId}, \n"+ 
+        //        $"Pen Type is {PenType}, \n"+
+        //        $"Pen Color is {PenColor}, \n" +
+        //        $"Pen Width is {PenWidth}");
+        //}
+
+        private int PenWidth { get; set; }
+        private int PenHeight { get; set; }
+
+        //To set width value we need set method as its property is private and not accessible directly
+        public void setPenWidth(int PenWidth)
+        {
+            this.PenWidth = PenWidth;
+        }
+
+        //To retrive width value we need getter method 
+        public int getPenWidth()
+        {
+            return this.PenWidth;
+        }
+
+        //set method is called from here to make it constant
+        public int getPenHeight()
+        {
+            setPenHeight(32);
+            return this.PenHeight;
+
+        }
+        //set method is private so we restrict user to set any data here.
+        private void setPenHeight(int PenHeight)
+        {
+            this.PenHeight = PenHeight;            
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace OOPSConcept
     {
         static void Main(string[] args)
         {
+            Pen pen = new Pen();
+            pen.PenId = 1;
+            pen.PenType = "Ball";
+            pen.PenColor = "Blue";
+
+            pen.setPenWidth(24);          
+            penInfo();
+
+
+            void penInfo()
+            {
+                Console.WriteLine($"Pen Id is {pen.PenId}, \n" +
+                    $"Pen Type is {pen.PenType}, \n" +
+                    $"Pen Color is {pen.PenColor}, \n" +
+                    $"Pen Width is {pen.getPenWidth()},\n"+
+                    $"Pen Height is {pen.getPenHeight()}");
+            }
+            //pen.penInfo();
             Console.WriteLine("Hello World!");
+
         }
     }
 }
